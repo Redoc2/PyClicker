10f699efa41e7442be579aae31ca54936f0621be
DieZeitungsente <younes.haika@gmail.com>
2022-12-19 11:33:08 +0100

    Able to reset now, added database and optimized code

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import os
 import time
+def remove():
+    try:
+        os.remove(os.getcwd() + "/database.txt")
+        return 'Success'
+    except:
+        return 'Failed'
+def write(name, x, x2, val):
+    with open(name, x) as x2:
+        print(val, file=x2)
 try:
     l = open('database.txt', 'r')
     bal = int(l.read())
@@ -10,11 +19,17 @@ except:
         print(bal, file=f)
 # create database.txt when not avaliable
 while True:
-    os.remove(os.getcwd() + "/database.txt")
-    with open('database.txt', 'a') as f:
-        print(bal, file=f)
+    remove()
+    write('database.txt', 'a', 'f', bal)
     print(bal)
     inp = input()
     if inp == '':
         bal += 1
+    elif inp == 'reset':
+        inpr = input('Are you sure you want to reset? [y/n]: ')
+        if inpr == 'y':
+            remove()
+            write('database.txt', 'a', 'f', 0)
+            print('Succesfully resetted stats!\nPress enter to continue')
+            input()
     os.system('cls')
